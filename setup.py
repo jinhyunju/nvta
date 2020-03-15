e769dbe9ea531f76d3dcd751c3d9496bb339c9e0
jhju <jhju@illumina.com>
2020-03-15 13:53:40 -0700

    integrating pytest into setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,26 @@
+import sys
+
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = []
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 setup(name="nvta",
       version='0.1',
@@ -7,5 +29,8 @@ setup(name="nvta",
       author="Jin Hyun Ju",
       author_email="jinhyun.ju@gmail.com",
       packages=['nvta'],
-      install_requires=["intervaltree", "pytest"]
+      install_requires=["intervaltree"],
+      test_suite="pytest",
+      tests_require=["pytest"],
+      cmdclass = {'test': PyTest}
       )
